58961aa59cab75818b2d35bfbe3bf5533a180b3a
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2025-04-20 01:10:22 +0100

    l3-8 fixed

--- l3-8.hs
+++ l3-8.hs
@@ -8,10 +8,10 @@ instance ToLi Structure where
   li_ (Structure s) = Structure $ el "li" s
 
 instance ToLi String where
-  li_ = Structure . el "li" . escape
+  li_ = Structure . el "li"
 
 ul2_ :: [Structure] -> Structure
-ul2_ ((Structure x) : xs) = (\(Structure l) -> Structure $ el "ul" l) $ foldl' (\acc (Structure s) -> append_ (li_ s) acc) (li_ x) xs
+ul2_ ((Structure x) : xs) = (\(Structure l) -> Structure $ el "ul" l) $ foldl' (\acc (Structure s) -> append_ acc (li_ s)) (li_ x) xs
 
 ul_ :: [Structure] -> Structure
 ul_ = Structure . el "ul" . concatMap ((\(Structure s) -> s) . li_ . (\(Structure s) -> s))
@@ -21,3 +21,9 @@ ol_ = Structure . el "ol" . concatMap (getStructureString . li_ . getStructureSt
 
 code_ :: String -> Structure
 code_ = Structure . el "pre" . escape
+
+main :: IO ()
+main = do
+  putStrLn $ render $ html_ "hello" (ul_ [p_ "hello", h1_ "world"])
+  putStrLn $ render $ html_ "hello" (ol_ [p_ "hello", h1_ "world"])
+  putStrLn $ render $ html_ "hello" (ul2_ [p_ "hello", h1_ "world"])
